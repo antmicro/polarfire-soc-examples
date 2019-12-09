8b44587ebab11410b2e53c76c3d1471e0d19c205
Michael Gielda <mgielda@antmicro.com>
2019-12-09 07:27:52 -0800

    Extract part of the CI yml file to entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+mkdir -p artifacts
+cd pse-blinky/Debug/
+make
+cp pse-blinky.elf ../../artifacts
+cd ../../pse-freertos-lwip/Debug/
+make
+cp pse-freertos-lwip.elf ../../artifacts
+cd ../../pse-mustein-julia/Debug/
+make
+cp pse-mustein-julia.elf ../../artifacts
+cd ../.. && ls artifacts/
